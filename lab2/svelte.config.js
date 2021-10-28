67f3d66833696849c402acabdc282ef229ffe474
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2021-10-28 19:01:11 +0300

    fixes

--- lab2/svelte.config.js
+++ lab2/svelte.config.js
@@ -2,43 +2,43 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@import "src/variables.scss";'
-					}
-				}
-			},
-			server: {
-				proxy: {
-					'/api/sendmail': {
-						target: 'http://localhost:5001/lab2-e8ed8/us-central1/sendmail',
-						changeOrigin: true,
-						secure: false,
-						rewrite: (path) => path.replace(/^\/api/, '')
-					}
-				}
-			}
-		}
-	},
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+        vite: {
+            css: {
+                preprocessorOptions: {
+                    scss: {
+                        additionalData: '@import "src/variables.scss";'
+                    }
+                }
+            },
+            server: {
+                proxy: {
+                    '/api/sendmail': {
+                        target: 'http://localhost:5001/lab2-e8ed8/us-central1/sendmail',
+                        changeOrigin: true,
+                        secure: false,
+                        rewrite: (path) => path.replace(/^\/api/, '')
+                    }
+                }
+            }
+        }
+    },
 
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@import "src/variables.scss";'
-			}
-		})
-	]
+    preprocess: [
+        preprocess({
+            scss: {
+                prependData: '@import "src/variables.scss";'
+            }
+        })
+    ]
 };
 
 export default config;
