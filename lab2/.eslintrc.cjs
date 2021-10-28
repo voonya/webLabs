a2051b99e28b68900686d5385d24c6ccb385eaaa
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2021-10-28 18:36:40 +0300

    fixes

--- lab2/.eslintrc.cjs
+++ lab2/.eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
 	root: true,
 	extends: ['eslint:recommended', 'prettier'],
 	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2019
